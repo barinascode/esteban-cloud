e2e9dfa82efe4aaf17916f4b9146762da5d8f788
Leonardo Tapia <barinas.code@gmail.com>
2021-03-28 04:41:44 +0000

    SSL support

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+const fs = require("fs")
+const http = require('http');
+const https = require('https');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/apiprod.ruzh.mx/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/apiprod.ruzh.mx/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/apiprod.ruzh.mx/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+
+
+
+
 
 
 const server = async ({ app, express,  jwt, mongoose, loaders, routes }) => {
@@ -6,14 +23,18 @@ const server = async ({ app, express,  jwt, mongoose, loaders, routes }) => {
     const { handlersControllers }  = await loaders({ app, express, mongoose, jwt })
 
     await routes({ app, handlersControllers })
+        
+    // Starting both http & https servers
+        // const httpServer = http.createServer(app);
+        const httpsServer = https.createServer(credentials, app);
 
-    app.listen( 80 , err => {
-            if (err) {
-            console.log(err);
-            return;
-            }
-            console.log(`Your server is ready !`);
-        });
+        // httpServer.listen(12000, () => {
+        //     console.log('HTTP Server running on port 80');
+        // });
+        const port = 3200
+        httpsServer.listen(port, () => {    
+            console.log(`HTTPS Images cloud runing on port ${port}`);
+        })
     }
 
 module.exports = server
